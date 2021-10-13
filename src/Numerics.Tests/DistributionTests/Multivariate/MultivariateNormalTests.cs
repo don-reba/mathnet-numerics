894c040c6c08e45c42794d2233254ecff9649aa4
Alexey Badalov <alexey.badalov@gmail.com>
2021-10-13 11:19:39 -0700

    Added the multivariate normal distribution.

---
+++ src/Numerics.Tests/DistributionTests/Multivariate/MultivariateNormalTests.cs
@@ -0,0 +1,223 @@
+﻿// <copyright file="MultinomialTests.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+//
+// Copyright (c) 2009-2016 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+using System;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Random;
+using NUnit.Framework;
+
+using SCG = System.Collections.Generic;
+
+namespace MathNet.Numerics.UnitTests.DistributionTests.Multivariate
+{
+    /// <summary>
+    /// Multinomial distribution tests.
+    /// </summary>
+    [TestFixture, Category("Distributions"), TestOf("MultivariateNormal")]
+    public class MultivariateNormalTests
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(int.MaxValue)]
+        public void ValidStandardDistributionParameters(int dim)
+        {
+            Assert.IsTrue(MultivariateNormal.IsValidParameterSet(dim));
+        }
+
+        [TestCase(-1)]
+        public void InalidStandardDistributionParameters(int dim)
+        {
+            Assert.IsFalse(MultivariateNormal.IsValidParameterSet(dim));
+        }
+
+        [Test]
+        public void CanCreateStandardDistribution()
+        {
+            var distribution = new MultivariateNormal(2);
+            Assert.AreEqual(Vector<double>.Build.Dense(2), distribution.Mean);
+            Assert.AreEqual(Matrix<double>.Build.DiagonalIdentity(2), distribution.Covariance);
+        }
+
+        [TestCase(new double[] { }, new double[] { })]
+        [TestCase(new double[] { 0 }, new double[] { 0 })]
+        [TestCase(new double[] { -1 }, new double[] { 0 })]
+        [TestCase(new double[] { 0, 0 }, new double[] { 0, 0, 0, 0 })]
+        [TestCase(new double[] { 1, -1 }, new double[] { 1, 0, 0, 1 })]
+        public void ValidGeneralDistributionParameters(double[] mean, double[] cov)
+        {
+            Assert.IsTrue(MultivariateNormal.IsValidParameterSet(
+                Vector<double>.Build.DenseOfArray(mean),
+                Matrix<double>.Build.DenseOfRowMajor(mean.Length, mean.Length, cov)));
+        }
+
+        [TestCase(new double[] { 0, 0 }, new double[] { 0, 1, 0, 0 }, Description = "Non-symmetric.")]
+        public void InvalidGeneralDistributionParameters(double[] mean, double[] cov)
+        {
+            Assert.IsFalse(MultivariateNormal.IsValidParameterSet(
+                Vector<double>.Build.DenseOfArray(mean),
+                Matrix<double>.Build.DenseOfRowMajor(mean.Length, mean.Length, cov)));
+        }
+
+        [TestCase(1, 0, 0)]
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 1, 2)]
+        [TestCase(1, 2, 1)]
+        public void InvalidGeneralDistributionParametersWithMismatchedDimensions(int meanDim, int covRows, int covCols)
+        {
+            Assert.IsFalse(MultivariateNormal.IsValidParameterSet(
+                Vector<double>.Build.Dense(meanDim),
+                Matrix<double>.Build.Dense(covRows, covCols)));
+        }
+
+        [Test]
+        public void CanCreateGeneralDistribution()
+        {
+            var mean = Vector<double>.Build.DenseOfArray(new double[] { 1.0, 2.5 });
+            var covariance = Matrix<double>.Build.DenseOfArray(new double[,] { { 2.0, 1.0 }, { 1.0, 3.0 } });
+            var distribution = new MultivariateNormal(mean, covariance);
+            Assert.AreEqual(mean, distribution.Mean);
+            Assert.AreEqual(covariance, distribution.Covariance);
+        }
+
+        [TestCase(new double[] { }, new double[] { })]
+        [TestCase(new double[] { 2.0 }, new double[] { 0.5 })]
+        [TestCase(new double[] { 2.0, 0.0, 0.0, 4.0 }, new double[] { 0.5, 0.0, 0.0, 0.25 })]
+        public void ValidatePrecision(double[] cov, double[] precision)
+        {
+            var dim = (int)Math.Sqrt(cov.Length);
+            var distribution = new MultivariateNormal(
+                Vector<double>.Build.Dense(dim),
+                Matrix<double>.Build.DenseOfRowMajor(dim, dim, cov));
+            AssertHelpers.AlmostEqual(
+                Matrix<double>.Build.DenseOfRowMajor(dim, dim, precision),
+                distribution.Precision, 4);
+        }
+
+        // let dnorm() be the standard normal probability density function
+        [TestCase(new double[] { 0 }, new double[] { 1 }, new double[] { 0 }, 0.3989422804014327)] // dnorm(0)
+        [TestCase(new double[] { 0 }, new double[] { 1 }, new double[] { 1 }, 0.24197072451914337)] // dnorm(1)
+        [TestCase(new double[] { 0 }, new double[] { 1 }, new double[] { 2 }, 0.05399096651318806)] // dnorm(2)
+        [TestCase(new double[] { -1, -1 }, new double[] { 2, -1, -1, 2 }, new double[] { 0, 0 }, 0.033803760991572902)] // dnorm(√2) / √6π
+        public void ValidateDensity(double[] mean, double[] cov, double[] x, double density)
+        {
+            var distribution = new MultivariateNormal(
+                Vector<double>.Build.DenseOfArray(mean),
+                Matrix<double>.Build.DenseOfRowMajor(mean.Length, mean.Length, cov));
+            AssertHelpers.AlmostEqual(density, distribution.Density(Vector<double>.Build.DenseOfArray(x)));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void StandardAndGeneralSamplesAgree(int dim)
+        {
+            var standard = new MultivariateNormal(dim, new System.Random(0));
+            var general  = new MultivariateNormal(MultivariateNormal.StandardMean(dim), MultivariateNormal.StandardCovariance(dim), new System.Random(0));
+            for (int i = 0; i != 5; ++i)
+            {
+                AssertHelpers.AlmostEqual(
+                    standard.Sample().PointwiseAbs(),
+                    general.Sample().PointwiseAbs(), 7);
+            }
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void StandardAndGeneralSamplesAgreeStatic(int dim)
+        {
+            var standardRandom = new System.Random(0);
+            var generalRandom = new System.Random(0);
+            var mean = MultivariateNormal.StandardMean(dim);
+            var covariance = MultivariateNormal.StandardCovariance(dim);
+            for (int i = 0; i != 5; ++i)
+            {
+                AssertHelpers.AlmostEqual(
+                    MultivariateNormal.Sample(standardRandom, dim).PointwiseAbs(),
+                    MultivariateNormal.Sample(generalRandom, mean, covariance).PointwiseAbs(), 7);
+            }
+        }
+
+        [TestCase(1, Category = "LongRunning")]
+        [TestCase(2, Category = "LongRunning")]
+        [TestCase(3, Category = "LongRunning")]
+        public void ValidateSamples(int dim)
+        {
+            var samples = MultivariateNormal.Samples(new System.Random(0), dim).Take(500);
+            var (Value, Significance, Pass) = MultivariateNormal.HenzeZirklerTest(samples);
+            Assert.True(Pass, $"Value = {Value}; Significance = {Significance}");
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void SamplesAreDeterministic(int dim)
+        {
+            Assert.AreEqual(
+                MultivariateNormal.Samples(new System.Random(0), dim).Take(100),
+                MultivariateNormal.Samples(new System.Random(0), dim).Take(100));
+        }
+
+        [Test]
+        // Test against the R MVN implementation.
+        public void ValidateHenzeZerkelTest()
+        {
+            const int equalityDecimals = 7;
+
+            // the extra zero column tests singular covariance support
+            var irisSetosa = Matrix<double>.Build.DenseOfArray(new double[,] {
+                {5.1, 3.5, 1.4, .2, 0}, {4.9, 3.0, 1.4, .2, 0}, {4.7, 3.2, 1.3, .2, 0}, {4.6, 3.1, 1.5, .2, 0}, {5.0, 3.6, 1.4, .2, 0},
+                {5.4, 3.9, 1.7, .4, 0}, {4.6, 3.4, 1.4, .3, 0}, {5.0, 3.4, 1.5, .2, 0}, {4.4, 2.9, 1.4, .2, 0}, {4.9, 3.1, 1.5, .1, 0},
+                {5.4, 3.7, 1.5, .2, 0}, {4.8, 3.4, 1.6, .2, 0}, {4.8, 3.0, 1.4, .1, 0}, {4.3, 3.0, 1.1, .1, 0}, {5.8, 4.0, 1.2, .2, 0},
+                {5.7, 4.4, 1.5, .4, 0}, {5.4, 3.9, 1.3, .4, 0}, {5.1, 3.5, 1.4, .3, 0}, {5.7, 3.8, 1.7, .3, 0}, {5.1, 3.8, 1.5, .3, 0},
+                {5.4, 3.4, 1.7, .2, 0}, {5.1, 3.7, 1.5, .4, 0}, {4.6, 3.6, 1.0, .2, 0}, {5.1, 3.3, 1.7, .5, 0}, {4.8, 3.4, 1.9, .2, 0},
+                {5.0, 3.0, 1.6, .2, 0}, {5.0, 3.4, 1.6, .4, 0}, {5.2, 3.5, 1.5, .2, 0}, {5.2, 3.4, 1.4, .2, 0}, {4.7, 3.2, 1.6, .2, 0},
+                {4.8, 3.1, 1.6, .2, 0}, {5.4, 3.4, 1.5, .4, 0}, {5.2, 4.1, 1.5, .1, 0}, {5.5, 4.2, 1.4, .2, 0}, {4.9, 3.1, 1.5, .2, 0},
+                {5.0, 3.2, 1.2, .2, 0}, {5.5, 3.5, 1.3, .2, 0}, {4.9, 3.6, 1.4, .1, 0}, {4.4, 3.0, 1.3, .2, 0}, {5.1, 3.4, 1.5, .2, 0},
+                {5.0, 3.5, 1.3, .3, 0}, {4.5, 2.3, 1.3, .3, 0}, {4.4, 3.2, 1.3, .2, 0}, {5.0, 3.5, 1.6, .6, 0}, {5.1, 3.8, 1.9, .4, 0},
+                {4.8, 3.0, 1.4, .3, 0}, {5.1, 3.8, 1.6, .2, 0}, {4.6, 3.2, 1.4, .2, 0}, {5.3, 3.7, 1.5, .2, 0}, {5.0, 3.3, 1.4, .2, 0} }).EnumerateRows();
+
+            var (Value40, Significance40, Pass40) = MultivariateNormal.HenzeZirklerTest(irisSetosa.Take(40));
+
+            // compare to: mvn(iris[1:40, 1:4])
+            AssertHelpers.AlmostEqual(0.8562336, Value40, equalityDecimals);
+            AssertHelpers.AlmostEqual(0.1503585, Significance40, equalityDecimals);
+            Assert.That(Pass40, Is.True);
+
+            var (Value50, Significance50, Pass50) = MultivariateNormal.HenzeZirklerTest(irisSetosa);
+
+            // compare to: mvn(iris[1:50, 1:4])
+            AssertHelpers.AlmostEqual(0.9488453, Value50, equalityDecimals);
+            AssertHelpers.AlmostEqual(0.0499536, Significance50, equalityDecimals);
+            Assert.That(Pass50, Is.False);
+        }
+    }
+}
---
+++ src/Numerics/Distributions/MultivariateNormal.cs
@@ -0,0 +1,331 @@
+﻿// <copyright file="Multinomial.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+//
+// Copyright (c) 2009-2013 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Random;
+using MathNet.Numerics.Statistics;
+
+using static System.Math;
+
+namespace MathNet.Numerics.Distributions
+{
+    /// <summary>
+    /// Multivariate Normal distribution. For details about this distribution, see
+    /// <a href="https://en.wikipedia.org/wiki/Multivariate_normal_distribution">Wikipedia - Multivariate normal distribution</a>.
+    /// Supports degenerate (non-singular) covariance matrices.
+    /// </summary>
+    [DebuggerDisplay("{Mean.Count}D Normal")]
+    public class MultivariateNormal : IDistribution
+    {
+        System.Random _random;
+
+        readonly Vector<double> _mean;
+        readonly Matrix<double> _covariance;
+        readonly Matrix<double> _root;
+
+        public MultivariateNormal(Vector<double> mean, Matrix<double> covariance, System.Random randomSource)
+        {
+            if (!IsValidParameterSet(mean, covariance))
+            {
+                throw new ArgumentException("Invalid parametrization for the distribution.");
+            }
+
+            _random = randomSource;
+            _mean = mean.Clone();
+            _covariance = covariance.Clone();
+            _root = CovarianceRoot(covariance);
+        }
+
+        /// <summary>
+        /// Initializes a standard multivariate normal distribution with a zero mean and an identity covariance.
+        /// </summary>
+        public MultivariateNormal(int dimensions)
+            : this(dimensions, SystemRandomSource.Default)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a standard multivariate normal distribution with a zero mean and an identity covariance.
+        /// </summary>
+        public MultivariateNormal(int dimensions, System.Random randomSource)
+        {
+            if (!IsValidParameterSet(dimensions))
+            {
+                throw new ArgumentException("Invalid parametrization for the distribution.");
+            }
+
+            _random = randomSource;
+            _mean = StandardMean(dimensions);
+            _covariance = StandardCovariance(dimensions);
+            _root = _covariance;
+
+        }
+
+        public MultivariateNormal(Vector<double> mean, Matrix<double> covariance)
+            : this(mean, covariance, SystemRandomSource.Default)
+        {
+        }
+
+        /// <summary>
+        /// A string representation of the distribution.
+        /// </summary>
+        /// <returns>a string representation of the distribution.</returns>
+        public override string ToString()
+        {
+            return $"Normal(μ = {_mean}, Σ = {_covariance})";
+        }
+
+        /// <summary>
+        /// Tests whether the provided values are valid parameters for a standard multivariate normal distribution.
+        /// </summary>
+        public static bool IsValidParameterSet(int dimensions)
+        {
+            return dimensions >= 0;
+        }
+
+        /// <summary>
+        /// Tests whether the provided values are valid parameters for a multivariate normal distribution.
+        /// </summary>
+        public static bool IsValidParameterSet(Vector<double> mean, Matrix<double> covariance)
+        {
+            if (mean == null || covariance == null)
+                return false;
+
+            // dimensions must agree
+            if (mean.Count != covariance.RowCount || mean.Count != covariance.ColumnCount)
+                return false;
+
+            // need no further checks for a zero-dimensional distribution
+            if (mean.Count == 0)
+                return true;
+
+            // covariance must be symmetric
+            if (!covariance.IsSymmetric())
+                return false;
+
+            return true;
+        }
+
+        public Vector<double> Mean => _mean;
+
+        public Vector<double> Mode => _mean;
+
+        public Matrix<double> Covariance => _covariance;
+
+        /// <summary>
+        /// Gets the multivariate normal precision, equal to the inverse of the covariance.
+        /// </summary>
+        public Matrix<double> Precision => _covariance.Inverse();
+
+        /// <summary>
+        /// Computes the probability density of the distribution (PDF) at x, equal to exp(-½(x-μ)ᵀΣ⁻¹(x-μ)) / sqrt(det(Σ)(2π)^k). Defined only if the covariance is positive definite.
+        /// </summary>
+        public double Density(Vector<double> x)
+        {
+            return Exp(-0.5 * (x - _mean) * _covariance.Inverse() * (x - _mean))
+                / Sqrt(_covariance.Determinant() * Pow(2.0 * PI, _mean.Count));
+        }
+
+        /// <summary>
+        /// Gets or sets the random number generator which is used to draw random samples.
+        /// </summary>
+        public System.Random RandomSource
+        {
+            get => _random;
+            set => _random = value ?? SystemRandomSource.Default;
+        }
+
+        /// <summary>
+        /// Samples a multivariate normal random variable.
+        /// </summary>
+        /// <returns>the counts for each of the different possible values.</returns>
+        public Vector<double> Sample()
+        {
+            return SampleFromRoot(_random, _mean, _root);
+        }
+
+        /// <summary>
+        /// Generates a sequence of samples from a multivariate normal variable.
+        /// </summary>
+        public IEnumerable<Vector<double>> Samples()
+        {
+            while (true)
+            {
+                yield return SampleFromRoot(_random, _mean, _root);
+            }
+        }
+
+        /// <summary>
+        /// Computes a square root A of the matrix M, such that AᵀA = M.
+        /// </summary>
+        private static Matrix<double> CovarianceRoot(Matrix<double> m)
+        {
+            if (m.ColumnCount == 0 && m.RowCount == 0)
+                return m;
+            var svd = m.Svd(true);
+            return svd.U * svd.W.PointwiseSqrt();
+        }
+
+        private static Vector<double> SampleFromRoot(System.Random rnd, Vector<double> mean, Matrix<double> root)
+        {
+            if (mean.Count == 0)
+                return mean;
+
+            // choose x from the standard multivariate normal distribution
+            var x = Vector<double>.Build.SameAs(mean);
+            for (int i = 0; i != x.Count; ++i)
+                x[i] = Normal.Sample(rnd, 0.0, 1.0);
+
+            return mean + root * x;
+        }
+
+        /// <summary>
+        /// Samples a multivariate normal random variable.
+        /// </summary>
+        public static Vector<double> Sample(System.Random rnd, Vector<double> mean, Matrix<double> covariance)
+        {
+            return SampleFromRoot(rnd, mean, CovarianceRoot(covariance));
+        }
+
+        /// <summary>
+        /// Samples a standard multivariate normal random variable.
+        /// </summary>
+        /// <remarks>Computationally slightly more efficient than the general case.</remarks>
+        public static Vector<double> Sample(System.Random rnd, int dimensions)
+        {
+            var x = Vector<double>.Build.Dense(dimensions);
+
+            for (int i = 0; i != x.Count; ++i)
+                x[i] = Normal.Sample(rnd, 0.0, 1.0);
+
+            return x;
+        }
+
+        /// <summary>
+        /// Generates a sequence of samples from a multivariate normal variable.
+        /// </summary>
+        /// <returns>a sequence of counts for each of the different possible values.</returns>
+        public static IEnumerable<Vector<double>> Samples(System.Random rnd, Vector<double> mean, Matrix<double> covariance)
+        {
+            while (true)
+            {
+                yield return Sample(rnd, mean, covariance);
+            }
+        }
+
+        /// <summary>
+        /// Generates a sequence of samples from a standard multivariate normal variable.
+        /// </summary>
+        public static IEnumerable<Vector<double>> Samples(System.Random rnd, int dimensions)
+        {
+            while (true)
+            {
+                yield return Sample(rnd, dimensions);
+            }
+        }
+
+        public static Vector<double> StandardMean(int dimensions)
+        {
+            return Vector<double>.Build.Dense(dimensions);
+        }
+
+        public static Matrix<double> StandardCovariance(int dimensions)
+        {
+            return Matrix<double>.Build.DiagonalIdentity(dimensions);
+        }
+
+        /// <summary>
+        /// Multivariate normality test using the Henze-Zerkel's test statistic.
+        /// N. Henze, B. Zirkler - A class of invariant and consistent tests for multivariate normality - 1990
+        /// </summary>
+        /// <see href="link">https://cran.r-project.org/package=MVN</see>
+        /// <remarks>This is a good default test because of its affine invariance and consistency. This is to say that an affine transformation of the input does not change the test's outcome, and, given enough data, the test will reject any distribution other than the multivariate normal.</remarks>
+        /// <returns></returns>
+        public static (double Value, double Significance, bool Pass) HenzeZirklerTest(IEnumerable<Vector<double>> samples)
+        {
+            double n = samples.Count();
+
+            var (m, cov) = samples.MeanCovariance();
+
+            var covInv = ((n - 1) * cov / n).PseudoInverse();
+            double p = covInv.Rank();
+ 
+            // smoothing parameter
+            double b = Constants.Sqrt1Over2 * Pow((p + p + 1) / 4, 1 / (p + 4)) * Pow(n, 1 / (p + 4));
+
+            // test statistic
+
+            double sum1 = 0;
+            foreach (var sampleI in samples)
+            foreach (var sampleJ in samples)
+            {
+                var diff = sampleI - sampleJ;
+                double scaledResidual = diff * covInv * diff;
+                sum1 += Exp(-0.5 * b * b * scaledResidual);
+            }
+
+            double sum2 = 0;
+            foreach (var sample in samples)
+            {
+                var diff = sample - m;
+                double scaledResidual = diff * covInv * diff;
+                sum2 += Exp(-0.5 * b * b * scaledResidual / (1 + b * b));
+            }
+
+            double hz = n * (
+                    1 / n / n * sum1 -
+                    2 * Pow(1 + b * b, -0.5 * p) * 1 / n * sum2 +
+                    Pow(1 + 2 * b * b, -0.5 * p));
+
+            // statistic distribution
+
+            double wb = (1 + b * b) * (1 + 3 * b * b);
+
+            double a = 1 + 2 * b * b;
+
+            double hzMean = 1 - Pow(a, -0.5 * p) * (1 + p * b * b / a + p * (p + 2) * Pow(b, 4) / (2 * a * a));
+            double hzVar =
+                2 * Pow(1 + 4 * b * b, -0.5 * p) +
+                2 * Pow(a, -p) * (1 + 2 * p * Pow(b, 4) / a / a + 3 * p * (p + 2) * Pow(b, 8) / 4 / Pow(a, 4)) -
+                4 * Pow(wb, -0.5 * p) * (1 + 3 * p * Pow(b, 4) / (2 * wb) + p * (p + 2) * Pow(b, 8) / (2 * wb * wb));
+
+            // log-Normal distribution
+
+            double logNormalHzMean = Log(Sqrt(Pow(hzMean, 4) / (hzVar + hzMean * hzMean)));
+            double logNormalHzStdDev = Sqrt(Log((hzVar + hzMean * hzMean) / hzMean / hzMean));
+            double pValue = 1 - LogNormal.CDF(logNormalHzMean, logNormalHzStdDev, hz);
+
+            return (hz, pValue, pValue > 0.05);
+        }
+    }
+}
